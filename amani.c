332917fe7b453daa4cfd94667d8bc12841aae471
Amani773 <116097755+Amani773@users.noreply.github.com>
2022-10-18 17:43:37 +0100

    Add files via upload

---
+++ amani.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+int x1,x2;
+printf("valeur du premier entier:");
+scanf("%d",&x1);
+printf("valeur du deuxieme entier:");
+scanf("%d",&x2);
+printf("le produit de %d et %d =%d",x1,x2,x1*x2);
+}
